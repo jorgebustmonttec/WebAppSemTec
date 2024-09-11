e5c92cdffed12d35aca27dc133693b95ca4d68f4
EugenioPedraza <a00835064@tec.mx>
2024-09-11 17:02:28 -0600

    search bar v1.1

--- src/app/components/searchBar.jsx
+++ src/app/components/searchBar.jsx
@@ -1,8 +1,10 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import { getSearchItems } from '../lib/spotify'; 
+import { getSearchItems, getPlaylists, addTrackToPlaylist } from '../lib/spotify'; 
 import { useSession } from 'next-auth/react';
 import { Input } from "@nextui-org/input";
+import {Image} from '@nextui-org/react';
+import { Button } from '@nextui-org/react';
 
 const SearchBar = () => {
   const { data: session } = useSession();
@@ -10,18 +12,16 @@ const SearchBar = () => {
   const [query, setQuery] = useState('');
   const [debouncedQuery, setDebouncedQuery] = useState(query);
 
-  // Debounce the query to prevent frequent API calls
   useEffect(() => {
     const handler = setTimeout(() => {
       setDebouncedQuery(query);
-    }, 500); // Delay in milliseconds before setting the debounced query
+    }, 500); 
 
     return () => {
       clearTimeout(handler);
     };
   }, [query]);
 
-  // Fetch search results when the debounced query or session token changes
   useEffect(() => {
     const handleSearch = async () => {
       if (!debouncedQuery || !session?.accessToken) {
@@ -40,7 +40,7 @@ const SearchBar = () => {
   }, [debouncedQuery, session?.accessToken]);
 
   return (
-    <div className="grid grid-cols-2 align-middle mt-11">
+    <div className="grid grid-cols-2 mt-11">
       <div className="w-60 h-20 col-span-1">
         <Input
           type="text"
@@ -48,15 +48,20 @@ const SearchBar = () => {
           value={query}
           variant="underlined"
           onChange={(e) => setQuery(e.target.value)}
-          className="bg-gray-800 text-white p-2 rounded"
         />
       </div>
 
       <div className="grid col-span-2 h-60 overflow-y-auto">
         {searchResults.length > 0 ? (
           searchResults.map((track) => (
-            <div key={track.id} className="text-white mb-2">
-              <p>{track.name} by {track.artists[0].name}</p>
+            <div key={track.id} className="text-white mb-2 grid grid-cols-3 items-center justify-items-start">
+              <Image className="w-20 h-20 col-span-1" src={track.album.images[0].url} alt="Album cover" />
+              <p className="italic font-bold col-span-1">{track.name} by {track.artists[0].name}</p>
+              <div className="flex justify-end">
+                <Button className="w-32 ml-2" id={track.id} color="warning" onClick={() => console.log('Track added to playlist')}>
+                  Add to playlist
+                </Button>
+              </div>
             </div>
           ))
         ) : (
--- src/app/lib/spotify.js
+++ src/app/lib/spotify.js
@@ -56,3 +56,32 @@ export async function getSearchItems(token, query) {
     }
   });
 }
+
+export async function getPlaylists(token) {
+  try {
+    const response = await axios.get(`${API_BASE_URL}/me/playlists`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data; 
+  } catch (error) {
+    console.error("Error fetching playlists:", error);
+    throw error;
+  }
+}
+
+export async function addTrackToPlaylist(token, playlistId, trackId) {
+  return axios.post(
+    `${API_BASE_URL}/playlists/${playlistId}/${trackId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        playlistId: playlistId,
+        trackId: trackId,
+      }
+    }
+  );
+}
